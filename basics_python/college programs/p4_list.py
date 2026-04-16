aa3fbe9af3ced1371a79877d90d046be90a13662
venkateshjunnur-boop <venkateshjunnur@gmail.com>
2026-04-16 12:43:15 +0000

    adding new three programs

---
+++ basics_python/college programs/p4_list.py
@@ -0,0 +1,15 @@
+list =[10,20,10]
+list.append(30) #add the end of the list 
+print(list) 
+list.insert(3,50) #add on the the starting on the list 
+print(list)
+list.remove(10) #remove perticular element on the list
+print(list)
+k =  list.pop() #remove the elment on the wnd of the list 
+print(k)
+list.count(10) #count the element repitation appears 
+print(list)
+list.clear() #clears the all elments in list 
+print(list)
+print(list.sort==reversed) #sort the list elment on reverse order 
+print(list)
